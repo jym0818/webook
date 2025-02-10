230dc11d236f6061613f3c8c76fd734d96ef00c4
贾越明 <12289853+jia-yueming@user.noreply.gitee.com>
2025-02-10 20:01:50 +0800

    注册功能完成到跨域

--- internal/web/user.go
+++ internal/web/user.go
@@ -1,20 +1,77 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	regexp "github.com/dlclark/regexp2"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+const emailRegexPattern = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"
+
+const passwordRegexPattern = `^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$`
 
 // UserHandler 表示与user相关的路由处理
 type UserHandler struct {
+	emailExp    *regexp.Regexp
+	passwordExp *regexp.Regexp
+}
+
+func NewUserHandler() *UserHandler {
+	return &UserHandler{
+		emailExp:    regexp.MustCompile(emailRegexPattern, regexp.None),
+		passwordExp: regexp.MustCompile(passwordRegexPattern, regexp.None),
+	}
 }
 
 func (u *UserHandler) RegisterRouters(s *gin.Engine) {
-	s.POST("/users/signup", u.Signup)
-	s.POST("/users/login", u.Login)
-	s.POST("/users/edit", u.Edit)
-	s.GET("/users/profile", u.Profile)
+	ug := s.Group("/users")
+	ug.POST("/signup", u.Signup)
+	ug.POST("/login", u.Login)
+	ug.POST("/edit", u.Edit)
+	ug.GET("/profile", u.Profile)
 }
 
 func (u *UserHandler) Signup(c *gin.Context) {
+	type SignUpReq struct {
+		Email           string `json:"email"`
+		Password        string `json:"password"`
+		ConfirmPassword string `json:"confirmPassword"`
+	}
+	var req SignUpReq
+	if err := c.Bind(&req); err != nil {
+		//记录日志 而不是把具体的错误返回给前端
+		c.JSON(http.StatusOK, "系统错误")
+		return
+	}
+	//参数校验  使用正则匹配
 
+	ok, err := u.emailExp.MatchString(req.Email)
+	//err不为空，说明正则表达式写错了，而不是匹配失败
+	if err != nil {
+		//记录日志，而不是返回具体错误给前端
+		c.JSON(http.StatusOK, "系统错误")
+		return
+	}
+	if !ok {
+		c.JSON(http.StatusOK, "邮箱格式不正确")
+		return
+	}
+
+	ok, err = u.passwordExp.MatchString(req.Password)
+	if err != nil {
+		//记录日志，而不是返回具体错误给前端
+		c.JSON(http.StatusOK, "系统错误")
+		return
+	}
+	if !ok {
+		c.JSON(http.StatusOK, "密码必须包含数字、特殊字符，并且长度不能小于 8 位")
+		return
+	}
+	if req.Password != req.ConfirmPassword {
+		c.JSON(http.StatusOK, "两次密码不同")
+		return
+	}
+	c.JSON(http.StatusOK, "注册成功")
 }
 func (u *UserHandler) Login(c *gin.Context) {
 
--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jym/webook/internal/web"
+	"strings"
+	"time"
 )
 
 func main() {
 	s := gin.Default()
-	u := &web.UserHandler{}
+	// 使用 CORS 中间件处理跨域问题，配置 CORS 参数
+	s.Use(cors.New(cors.Config{
+		// 允许的源地址（CORS中的Access-Control-Allow-Origin）
+		// AllowOrigins: []string{"https://foo.com"},
+		// 允许的 HTTP 方法（CORS中的Access-Control-Allow-Methods）
+		//如果省略，那么所有方法都允许
+		AllowMethods: []string{"PUT", "PATCH"},
+		// 允许的 HTTP 头部（CORS中的Access-Control-Allow-Headers）
+		AllowHeaders: []string{"Content-Type"},
+		// 暴露的 HTTP 头部（CORS中的Access-Control-Expose-Headers）
+		ExposeHeaders: []string{"Content-Length"},
+		// 是否允许携带身份凭证（CORS中的Access-Control-Allow-Credentials）
+		AllowCredentials: true,
+		// 允许源的自定义判断函数，返回true表示允许，false表示不允许
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				// 允许你的开发环境
+				return true
+			}
+			// 允许包含 "yourcompany.com" 的源
+			return strings.Contains(origin, "yourcompany.com")
+		},
+		// 用于缓存预检请求结果的最大时间（CORS中的Access-Control-Max-Age）
+		MaxAge: 12 * time.Hour,
+	}))
+	u := web.NewUserHandler()
 	u.RegisterRouters(s)
 	s.Run(":8080")
 }
