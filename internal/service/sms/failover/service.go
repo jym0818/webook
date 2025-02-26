2a6d14a1e92c2a387a63c8a4e74970639f5fc461
贾越明 <12289853+jia-yueming@user.noreply.gitee.com>
2025-02-26 15:42:53 +0800

    轮询-2

--- internal/service/sms/failover/service.go
+++ internal/service/sms/failover/service.go
@@ -4,24 +4,32 @@ import (
 	"context"
 	"errors"
 	"github.com/jym/webook/internal/service/sms"
-	"log"
+	"sync/atomic"
 )
 
 type FailoverSMSService struct {
 	svcs []sms.Service
+	idx  uint64
 }
 
-func (f FailoverSMSService) Send(ctx context.Context, tpl string, args []string, numbers ...string) error {
-	for _, svc := range f.svcs {
+func (f *FailoverSMSService) Send(ctx context.Context, tpl string, args []string, numbers ...string) error {
+	//我取下一个节点为起始节点
+	idx := atomic.AddUint64(&f.idx, 1)
+	length := uint64(len(f.svcs))
+	for i := idx; i < idx+length; i++ {
+		svc := f.svcs[int(i%length)]
 		err := svc.Send(ctx, tpl, args, numbers...)
-		//发送成功
-		if err == nil {
+		switch err {
+		case nil:
 			return nil
+		case context.DeadlineExceeded, context.Canceled: //超时可取消
+			return nil
+		default:
+			//输出日志和监控
+
 		}
-		//输出日志   要做好监控
-		log.Println("failover service send error:", err)
 	}
-	return errors.New("全部服务都失败了")
+	return errors.New("failover service ")
 }
 
 func NewFailoverSMSService(svcs []sms.Service) sms.Service {
