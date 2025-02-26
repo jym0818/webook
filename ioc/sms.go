4a80db07bb4b9a32f54f7dfdaa2a6084d9359a38
贾越明 <12289853+jia-yueming@user.noreply.gitee.com>
2025-02-26 14:50:34 +0800

    依赖注入修改--添加限流

--- ioc/sms.go
+++ ioc/sms.go
@@ -1,11 +1,17 @@
 package ioc
 
 import (
+	"github.com/jym/webook/internal/service/ratelimit"
 	"github.com/jym/webook/internal/service/sms"
 	"github.com/jym/webook/internal/service/sms/memory"
+	ratelimit2 "github.com/jym/webook/pkg/ratelimit"
+	"github.com/redis/go-redis/v9"
+	"time"
 )
 
 // 可以更改实现  memory或者tencent或者aliyun
-func InitSMSService() sms.Service {
-	return memory.NewService()
+func InitSMSService(cmd redis.Cmdable) sms.Service {
+	svc := memory.NewService()
+	limiter := ratelimit2.NewRedisSlidingWindowLimiter(cmd, time.Minute, 1000)
+	return ratelimit.NewRatelimitSMSService(svc, limiter)
 }
