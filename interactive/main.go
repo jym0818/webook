b15c2434b0222f18fd1065b153248b7d31318e68
贾越明 <12289853+jia-yueming@user.noreply.gitee.com>
2025-07-19 11:31:06 +0800

    修改bug

--- interactive/main.go
+++ interactive/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"log"
@@ -21,19 +19,10 @@ func main() {
 	log.Println(err)
 }
 func initViperV1() {
-	cfile := pflag.String("config",
-		"config/config.yaml", "指定配置文件路径")
+	cfgFile := pflag.String("config", "./config/config.yaml", "指定配置文件路径")
 	pflag.Parse()
-	viper.SetConfigFile(*cfile)
-	// 实时监听配置变更
-	viper.WatchConfig()
-	// 只能告诉你文件变了，不能告诉你，文件的哪些内容变了
-	viper.OnConfigChange(func(in fsnotify.Event) {
-		// 比较好的设计，它会在 in 里面告诉你变更前的数据，和变更后的数据
-		// 更好的设计是，它会直接告诉你差异。
-		fmt.Println(in.Name, in.Op)
-		fmt.Println(viper.GetString("db.dsn"))
-	})
+	viper.SetConfigFile(*cfgFile)
+
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(err)
